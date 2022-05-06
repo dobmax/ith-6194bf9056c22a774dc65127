152604a7576696f77939d49fed8a2e621bea50e7
wespe <m.dobrynin@raccoonsoft.ru>
2022-05-06 18:16:48 +0200

    concurrency: lesson-2 - cleanup

--- src/ua/ithillel/concurrency/classwork/BadPracticeThread.java
+++
@@ -1,8 +0,0 @@
-package ua.ithillel.concurrency.classwork;
-
-public class BadPracticeThread extends Thread {
-    @Override
-    public void run() {
-        System.out.println("Run badly!!!");
-    }
-}
--- src/ua/ithillel/concurrency/classwork/Counter.java
+++
@@ -1,6 +0,0 @@
-package ua.ithillel.concurrency.classwork;
-
-public interface Counter {
-    void increase();
-    int getVal();
-}
